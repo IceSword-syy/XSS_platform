03ba519b3b254dca1288d98fbbf67646c1018e89
lewangbtcc <le.wang@btcc.com>
2016-08-02 11:25:54 +0800

    Trivial modify

--- show/admin.py
+++ show/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from django.http import HttpResponse
+from receive.models import Cookie
+
+# Create your views here.
+
+def show_cookie(request):
+    '''
+    Store cookie information in database
+    '''
+
+    s = ''
+    for i in Cookie.objects.all():
+        s += '<li>' + i.cookie + ' ' + i.domain + ' ' + i.ip + '</li></br>'
+
+    return HttpResponse(s)
