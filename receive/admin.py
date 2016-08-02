934bb705da2a0772fd82afbf9fbd21f7fcdd5946
lewangbtcc <le.wang@btcc.com>
2016-08-02 11:24:11 +0800

    Add admin page

--- receive/admin.py
+++ receive/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Cookie
 
 # Register your models here.
+
+class CookieAdmin(admin.ModelAdmin):
+    list_display = ('date', 'domain', 'ip', 'update')
+
+admin.site.register(Cookie,CookieAdmin)
